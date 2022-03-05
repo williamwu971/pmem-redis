b760e27aab127f857bd93a93230937a275ec5e6e
William <xiwu9085@uni.sydney.edu.au>
2022-03-06 01:04:21 +1100

    -AUTO- Sun Mar  6 01:04:21 AEDT 2022

--- src/server.c
+++ src/server.c
@@ -4135,7 +4135,7 @@ int main(int argc, char **argv) {
 
     stop_timer
     double bw = (double) iter * 1000000. / (double) elapsed;
-    printf("bw %f/s\n",bw);
+    printf("bw %f/s elapsed %llus\n",bw,elapsed/1000000);
 
     //////////////// ends here ////////////////
 
