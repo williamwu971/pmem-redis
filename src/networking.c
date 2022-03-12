6ac2697ac4236dcc5c1352967ac641dfc313ad6e
William <xiwu9085@uni.sydney.edu.au>
2022-03-12 15:34:02 +1100

    -AUTO- Sat Mar 12 15:34:02 AEDT 2022

--- src/networking.c
+++ src/networking.c
@@ -903,7 +903,7 @@ ssize_t async_write(int fd, const void *buf, size_t count){
         return count;
     }else{
         printf("queue error! errno:%d\n",errno);
-        exit(1);
+        return 0;
     }
 
 }
