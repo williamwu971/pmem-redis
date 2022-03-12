a1c60402eee5c11807d344e7ac3dc588114cc0a8
William <xiwu9085@uni.sydney.edu.au>
2022-03-12 15:21:53 +1100

    -AUTO- Sat Mar 12 15:21:53 AEDT 2022

--- src/networking.c
+++ src/networking.c
@@ -1012,7 +1012,7 @@ void sendReplyToClient(aeEventLoop *el, int fd, void *privdata, int mask) {
     UNUSED(el);
     UNUSED(mask);
 
-    printf("sendReplyToClient\n"); //william
+//    printf("sendReplyToClient\n"); //william
 
     writeToClient(fd,privdata,1);
 }
