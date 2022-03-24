2490f93877787e319307c235187a45e31639cf71
William <xiwu9085@uni.sydney.edu.au>
2022-03-24 22:06:22 +1100

    -AUTO- Thu Mar 24 22:06:22 AEDT 2022

--- src/networking.c
+++ src/networking.c
@@ -889,7 +889,7 @@ void freeClientsInAsyncFreeQueue(void) {
 }
 
 #define WRITE_BATCH_SIZE 10
-#define FAST_WRITE write_by_write
+#define FAST_WRITE write_by_io
 
 ssize_t async_write(int fd, off_t offset,const void *buf, size_t count){
 
